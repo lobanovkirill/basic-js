f65835cfa762b860cf5fa09d53dae7ee8d62fb2a
lobanovkirill <kirill@lobanovkirill.ru>
2022-05-02 13:15:20 +0500

    feat: solved the task carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,43 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+
+  if(!checkIcomeDataType(sampleActivity)) return false;
+
+  const activity = Number(sampleActivity);
+  if(!checkNumber(activity)) return false;
+  
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  return Math.ceil(
+      (Math.log(MODERN_ACTIVITY / activity) / k)
+    ) || false;
+
+}
+
+function checkIcomeDataType(data) {
+  if(
+    typeof data !== 'string' ||
+    data === 'undefined' ||
+    data === '' ||
+    data === null
+  ) { 
+    return false
+  } else {
+    return true;
+  }
+}
+
+function checkNumber(data) {
+  if (data === 'NaN' ||
+      0 > data       ||
+      data > 15      ||
+      data === 0
+    ) {
+      return false
+  } else {
+      return data;
+  }
 }
 
 module.exports = {
